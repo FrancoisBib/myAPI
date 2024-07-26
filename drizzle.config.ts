073880a89ebf90b78dd27fc3fe778fa84e078577
Fraançois Bib <91905543+FrancoisBib@users.noreply.github.com>
2024-07-26 04:12:07 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
 import { Drizzle } from 'drizzle-orm';
 import { Pool } from 'pg';
 
-// Crée une instance de connexion à PostgreSQL
 const pool = new Pool({
   connectionString: 'postgres://<user>:<password>@<host>:<port>/<database>',
 });
 
-// Initialise Drizzle avec la connexion PostgreSQL
+// Initialisation de Drizzle avec la connexion PostgreSQL
 const drizzle = new Drizzle(pool);
 
 export default drizzle;
